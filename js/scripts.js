b2ebb57bcf46bb6c1e63e6c7292e86d4949ec623
Student <example@example.com>
2019-10-24 14:46:01 -0700

    Will update the calculator with radio buttons and branches

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-// Business (or back-end) logic:
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -16,35 +15,22 @@ var divide = function(number1, number2) {
 };
 
 
-//Everything below this line is user interface (or front-end) logic:
-
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
-   event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    var result = add(number1, number2);
-    $("#output").text(result);
-  });
-  $("form#divide").submit(function(event) {
-   event.preventDefault();
-    var number1 = parseInt($("#div1").val());
-    var number2 = parseInt($("#div2").val());
-    var result = divide(number1, number2);
-    $("#output").text(result);
-  });
-  $("form#subtract").submit(function(event) {
-   event.preventDefault();
-    var number1 = parseInt($("#sub1").val());
-    var number2 = parseInt($("#sub2").val());
-    var result = subtract(number1, number2);
-    $("#output").text(result);
-  });
-  $("form#multiply").submit(function(event) {
-   event.preventDefault();
-    var number1 = parseInt($("#multi1").val());
-    var number2 = parseInt($("#multi2").val());
-    var result = multiply(number1, number2);
+  $("form#calculator").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
     $("#output").text(result);
   });
 });
